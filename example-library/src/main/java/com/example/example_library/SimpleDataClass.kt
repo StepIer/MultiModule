d678a0f953a4eeda17ef3bcf427f04e71023e8ac
Stepan Kondratiev <Stepan.Kondratiev@softeq.com>
2022-07-25 11:35:59 +0300

    add new module

---
+++ example-library/src/main/java/com/example/example_library/SimpleDataClass.kt
@@ -0,0 +1,6 @@
+package com.example.example_library
+
+data class SimpleDataClass(
+    val value: Int = 1
+) {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "MultiModul"
 include(":app")
+include(":example-library")
